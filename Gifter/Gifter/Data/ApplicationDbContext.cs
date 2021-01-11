732ffde091099b1f1681768323954ab981be95aa
Goody3099 <mattgoodman3099@gmail.com>
2021-01-11 14:01:16 -0600

    A tank is a mode of transportation but not all modes of transportation are tanksgit add .!

---
+++ Gifter/Gifter/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Gifter.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gifter.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<UserProfile> UserProfile { get; set; }
+        public DbSet<Post> Post { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+    }
+}
---
+++ Gifter/Gifter/Repositories/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Gifter.Models;
+using System.Collections.Generic;
+
+namespace Gifter.Repositories
+{
+    public interface ICommentRepository
+    {
+        List<Comment> GetAll();
+        Comment GetById(int id);
+        void Add(Comment comment);
+        void Update(Comment comment);
+        void Delete(int id);
+    }
+}
